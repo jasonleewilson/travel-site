38f737240295f6fbceaf189ce7b372b04d0140cc
jasonleewilson <info@jasonleewilson.com>
2017-01-16 03:45:09 -0700

    added base and modules css updated index gulp package

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested');
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
   console.log('Horay you created a gulp task');
@@ -15,18 +17,30 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssvars, nested, autoprefixer]))
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
   watch('./app/index.html', function() {
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
 });
